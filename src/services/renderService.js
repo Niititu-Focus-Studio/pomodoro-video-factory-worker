1d859c4ed1b8dd6375d56a1e789e4a82336b6f74
Restu Averian <restuaverianputra123@gmail.com>
2026-07-14 07:58:20 +0700

    update

--- src/services/renderService.js
+++ src/services/renderService.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { executeRenderPipeline } = require("../../../shared/renderPipeline");
+const { executeRenderPipeline } = require("../shared/renderPipeline");
 
 const SAFE_ASSET = /^assets\/[A-Za-z0-9._ -]+$/;
 
